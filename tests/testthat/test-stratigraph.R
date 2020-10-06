d1819166c3d579ef1d21c2f5bf52bf85cfb143fb
Joe Roe <joe@joeroe.io>
2020-10-06 09:25:10 +0200

    Add tests for strat_is_valid()

---
+++ tests/testthat/test-stratigraph.R
@@ -0,0 +1,14 @@
+test_that("strat_is_valid() returns TRUE for the harris12 dataset", {
+  data("harris12")
+  h12_graph <- stratigraph(harris12, "context", "above")
+  expect_warning(strat_is_valid(h12_graph), NA)
+  expect_true(strat_is_valid(h12_graph))
+})
+
+test_that("strat_is_valid() detects cycles", {
+  data("harris12")
+  harris12$above[1] <- list(c(10))
+  h12_graph <- stratigraph(harris12, "context", "above")
+  expect_warning(strat_is_valid(h12_graph), "cycle")
+  expect_false(strat_is_valid(h12_graph))
+})
