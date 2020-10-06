2a2948d33d17780e7bb2898babb56af8de8cc65b
Joe Roe <joe@joeroe.io>
2020-10-06 17:52:30 +0200

    Add option to split multiple values to read_lst()

--- R/read.R
+++ R/read.R
@@ -8,15 +8,32 @@
 #' Reads stratigraphic data in LST format (used by BASP Harris, Stratify, and
 #'   ArchEd) into a data frame.
 #'
-#' @param file    Path to an LST file.
-#' @param locale  [readr::locale()] object specifying the character encoding and
-#'                 other region-specific settings used in the file. Defaults to
-#'                 [readr::default_locale()], which should be fine in most cases.
+#' @param file Path to an LST file.
+#' @param split Controls how attributes that contain multiple values are
+#'   handled. If `TRUE`, all attributes except `name` will be split if their
+#'   values contain the character specified by `sep`. If `FALSE`, no
+#'   splitting will be done. Alternatively, specify a vector of column names
+#'   (corresponding to LST attribute names, see details) that should be split.
+#' @param sep Delimiter used to separate multiple values in attributes.
+#'   multiple values. Ignored if `split = FALSE`. Default: ",".
+#' @param locale [readr::locale()] object specifying the character encoding and
+#'   other region-specific settings used in the file. Defaults to
+#'   [readr::default_locale()].
 #'
 #' @return
 #' A data frame. Each row represents one stratum. Columns contain the name of
-#'   the stratum and the attributes associated with it (typically "above",
-#'   "contemporary_with", "equal_to" and "below").
+#' the stratum and the attributes associated with it (typically "above",
+#' "contemporary_with", "equal_to" and "below").
+#'
+#' Attribute names are standardised when read into column names. Specifically,
+#' they are transformed to lower case and spaces are replaced with an underscore
+#' (_).
+#'
+#' Splitting multiple values into vectors is useful if the output is to be used
+#' to construct a stratigraphic graph with [stratigraph()], but will convert
+#' affected columns from atomic vectors to lists, which can make them awkward
+#' to work with in other contexts. Set `split = FALSE` to avoid this. You can
+#' manually split them later with [stringr::str_split()].
 #'
 #' @details
 #' This function supports both the original LST format used by BASP Harris and
@@ -38,7 +55,8 @@
 #' # Extended LST (Stratify, ArchEd)
 #' stratify_lst <- system.file("extdata", "stratify.lst", package = "stratigraphr")
 #' read_lst(stratify_lst)
-read_lst <- function(file, locale = readr::default_locale()) {
+read_lst <- function(file, split = TRUE, sep = ",",
+                     locale = readr::default_locale()) {
   # TODO: Add type checking/validation
   lst <- readr::read_lines(file, locale = locale)
 
@@ -50,8 +68,27 @@ read_lst <- function(file, locale = readr::default_locale()) {
   strata <- lst[4:length(lst)]
   strata <- split(strata, cumsum(stringr::str_detect(strata, "^\\s", negate = TRUE)))
 
-  # Extract strata as data frame and return
+  # Extract attributes for each stratum and bind into a data frame
   strata <- purrr::map_dfr(strata, lst_extract_stratum)
+
+  # Split multiple values into vectors if required
+  if (any(split != FALSE)) {
+    if (any(split == TRUE)) {
+      cols_to_split <- names(strata)
+      cols_to_split <- cols_to_split[cols_to_split != "name"]
+    }
+    else {
+      cols_to_split <- split
+    }
+
+    strata <- dplyr::mutate(strata, dplyr::across(dplyr::all_of(cols_to_split),
+       ~if (any(stringr::str_detect(.x, stringr::coll(sep)), na.rm = TRUE)) {
+         stringr::str_split(.x, stringr::coll(sep))
+       }
+       else {
+         return(.x)
+       }))
+  }
 
   attr(strata, "dataset_name") <- header$dataset_name
   return(strata)
@@ -80,28 +117,18 @@ lst_extract_header <- function(lst) {
 #' @param lst Character vector of lines from an LST file (e.g. from [readr::read_lines()])
 #'              representing a single stratum.
 #'
-#' @details
-#' Currently lst_extract_stratum() assumes each stratum is exactly five lines
-#'   long: the first line with the name of the stratum, and subsequent lines
-#'   containing its attributes (passed to [lst_extract_attribute()]). This
-#'   may not be a sound assumption for the "extended" format mentioned in
-#'   <http://archaeologic.al/wiki/Harris_Matrix#LST>.
-#'
 #' @return
-#' Data frame with one row and columns representing the name and attributes of
-#'   a stratum.
+#' A named list of attributes.
 #'
 #' @noRd
 lst_extract_stratum <- function(stratum) {
   # TODO: Add type checking
   # TODO: Extract name, label and site code separately – need some test data
   name <- stratum[1]
-  attrs <- purrr::map_dfc(stratum[2:length(stratum)], lst_extract_attribute)
+  attrs <- purrr::map(stratum[2:length(stratum)], lst_extract_attribute)
+  attrs <- purrr::flatten(attrs)
 
-  stratum <- cbind(
-    name = name,
-    attrs
-  )
+  stratum <- c(name = name, attrs)
   return(stratum)
 }
 
@@ -110,7 +137,7 @@ lst_extract_stratum <- function(stratum) {
 #' @param attr A single attribute line from an LST file.
 #'
 #' @return
-#' A named list containing the attribute name and value.
+#' A named list with one element, i.e. `list(attribute = value)`.
 #'
 #' @noRd
 lst_extract_attribute <- function(attr) {
@@ -132,13 +159,8 @@ lst_extract_attribute <- function(attr) {
 
   # Make missing values explicit
   if(value == "") {
-    value <- NA
+    value <- as.character(NA)
   }
-
-  # Vectorise multiple attribute values (if value is not missing)
-  # else if(stringr::str_detect(value, stringr::coll(","))) {
-  #   value <- stringr::str_split(value, stringr::coll(","))
-  # }
 
   attr <- list(value)
   names(attr) <- name
