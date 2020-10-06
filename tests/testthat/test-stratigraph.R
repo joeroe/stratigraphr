45ffcbb08a27895e811f6d6a264c8ee510d77290
Joe Roe <joe@joeroe.io>
2020-10-06 11:45:37 +0200

    Update strat_is_valid() cycle test for new harris12 dataset

--- tests/testthat/test-stratigraph.R
+++ tests/testthat/test-stratigraph.R
@@ -7,7 +7,7 @@ test_that("strat_is_valid() returns TRUE for the harris12 dataset", {
 
 test_that("strat_is_valid() detects cycles", {
   data("harris12")
-  harris12$above[1] <- list(c(10))
+  harris12$above[1] <- list(c("natural"))
   h12_graph <- stratigraph(harris12, "context", "above")
   expect_warning(strat_is_valid(h12_graph), "cycle")
   expect_false(strat_is_valid(h12_graph))
