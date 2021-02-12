d11af087ea8fa24cf72b542621979fb0f1c21d3d
Joe Roe <joe@joeroe.io>
2021-02-12 09:46:27 +0100

    Fix stratigraph tests

--- tests/testthat/test-stratigraph.R
+++ tests/testthat/test-stratigraph.R
@@ -18,7 +18,7 @@ test_that("strat_is_valid() returns TRUE for the harris12 dataset", {
 test_that("strat_is_valid() detects cycles", {
   data("harris12")
   harris12$above[1] <- list(c("natural"))
-  h12_graph <- stratigraph(harris12, "context", "above")
+  expect_warning(h12_graph <- stratigraph(harris12, "context", "above"))
   expect_warning(strat_is_valid(h12_graph), "cycle")
-  expect_false(strat_is_valid(h12_graph))
+  expect_false(strat_is_valid(h12_graph, warn = FALSE))
 })
